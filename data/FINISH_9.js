c48156725d2b0fd70033d3aaac4c3151bed6ed34
GASENG27 <150409393+GASENG27@users.noreply.github.com>
2023-11-15 17:16:17 +0700

    Add files via upload

---
+++ data/FINISH_9.js
@@ -0,0 +1,1 @@
+var json_FINISH_9 = {"type":"FeatureCollection","name":"FINISH_9","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","FINISH":"finish1"},"geometry":{"type":"Point","coordinates":[102.890366245224229,-3.27803165209222]}}]}
---
+++ data/StandingWave_4.js
@@ -0,0 +1,1 @@
+var json_StandingWave_4 = {"type":"FeatureCollection","name":"StandingWave_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","standing":"standing wave1"},"geometry":{"type":"Point","coordinates":[102.826420838581697,-3.318904571437819]}}]}
